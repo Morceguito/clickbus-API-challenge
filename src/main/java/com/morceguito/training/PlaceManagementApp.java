9b6e04460c2b76f7633967925f06154a76599998
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-12 15:55:38 +0000

    refactor: renaming main class and package

--- src/main/java/com/morceguito/training/PlaceManagementApp.java
+++ src/main/java/com/morceguito/training/PlaceManagementApp.java
@@ -1,6 +1,6 @@
-package org.example;
+package com.morceguito.training;
 
-public class Main {
+public class PlaceManagementApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
