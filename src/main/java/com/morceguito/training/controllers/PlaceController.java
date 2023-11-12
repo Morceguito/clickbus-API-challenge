cdba4d4371ef16757879b665ae2339cf3f72d810
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-12 19:31:04 +0000

    fix: add requestbody to controller and response

--- src/main/java/com/morceguito/training/controllers/PlaceController.java
+++ src/main/java/com/morceguito/training/controllers/PlaceController.java
@@ -1,10 +1,13 @@
 package com.morceguito.training.controllers;
 
-import com.morceguito.training.entities.Place;
+import com.morceguito.training.dtos.PlaceRequest;
+import com.morceguito.training.dtos.PlaceResponse;
+import com.morceguito.training.mappers.PlaceMapper;
 import com.morceguito.training.services.PlaceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -20,8 +23,8 @@ public class PlaceController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<Place>> create(Place place) {
-        Mono<Place> createdPlace = placeService.createPlace(place);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+    public ResponseEntity<Mono<PlaceResponse>> create(@RequestBody PlaceRequest place) {
+        Mono<PlaceResponse> createdPlaceResponse = placeService.createPlace(place).map(PlaceMapper::fromPlaceToResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlaceResponse);
     }
 }
