0e9428d505f757b258c9a13f1615404a2258e3ba
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-12 19:32:33 +0000

    fix: service now uses request dto

--- src/main/java/com/morceguito/training/services/PlaceService.java
+++ src/main/java/com/morceguito/training/services/PlaceService.java
@@ -1,5 +1,6 @@
 package com.morceguito.training.services;
 
+import com.morceguito.training.dtos.PlaceRequest;
 import com.morceguito.training.entities.Place;
 import com.morceguito.training.repositories.PlaceRepository;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,9 @@ public class PlaceService {
         this.placeRepository = placeRepository;
     }
 
-    public Mono<Place> createPlace(Place place){
+    public Mono<Place> createPlace(PlaceRequest placeRequest){
+        Place place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(),
+                placeRequest.createdAt(),placeRequest.updatedAt());
         return placeRepository.save(place);
     }
 
