a88c282097ca2d408cc2e22e2856dd40b7d46a82
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-12 19:33:02 +0000

    fix: missing spring boot configurations

--- src/main/java/com/morceguito/training/PlaceManagementApp.java
+++ src/main/java/com/morceguito/training/PlaceManagementApp.java
@@ -1,7 +1,11 @@
 package com.morceguito.training;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PlaceManagementApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(PlaceManagementApp.class, args);
     }
 }
