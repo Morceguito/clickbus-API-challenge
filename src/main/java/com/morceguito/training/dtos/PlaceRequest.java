cdfe7c3e08128849383c2c46f96cc15406b53cda
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-13 00:16:01 +0000

    feat: validation of name and state to not be blank

--- src/main/java/com/morceguito/training/dtos/PlaceRequest.java
+++ src/main/java/com/morceguito/training/dtos/PlaceRequest.java
@@ -1,8 +1,10 @@
 package com.morceguito.training.dtos;
 
 
+import jakarta.validation.constraints.NotBlank;
+
 public record PlaceRequest (
-        String name,
-        String state
+        @NotBlank String name,
+        @NotBlank String state
 ) {
 }
